f54d29ed8e7e736dcf13af1108cd9065740a31ef
uraniniteresidue <2549847059@qq.com>
2026-03-28 01:12:15 +0800

    mana and terra hooks fix ): (

--- src/main/java/slimeknights/tconstruct/tools/modifiers/Botania/TerraRecoverModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/Botania/TerraRecoverModifier.java
@@ -6,9 +6,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.hook.interaction.InventoryTickModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
-import vazkii.botania.api.mana.ManaItem;
 import vazkii.botania.api.mana.ManaItemHandler;
 
 public class TerraRecoverModifier extends Modifier implements InventoryTickModifierHook {
@@ -18,25 +19,16 @@ public class TerraRecoverModifier extends Modifier implements InventoryTickModif
     public TerraRecoverModifier() {
     }
     @Override
-    public void onInventoryTick(IToolStackView tool, ModifierEntry modifier, Level level, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack itemStack) {
-        if (level.isClientSide || !(holder instanceof Player player)) return;
-        boolean success =false;
+    protected final void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        hookBuilder.addHook(this, ModifierHooks.INVENTORY_TICK);
+    }
+    @Override
+    public void onInventoryTick(IToolStackView tool, ModifierEntry modifier, Level level, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack stack) {
+        if (level.isClientSide || !(holder instanceof Player player)) {
+            return;
+        }
         if (level.getGameTime() % HEAL_INTERVAL == 0 && player.getHealth() < player.getMaxHealth()) {
-
-            for (ItemStack stack : ManaItemHandler.instance().getManaAccesories(player)) {
-                if (!stack.isEmpty() && stack.getItem() instanceof ManaItem manaItem
-                        && manaItem.canReceiveManaFromItem(stack)) {
-                    success = ManaItemHandler.instance().dispatchManaExact(stack, player, MANA * modifier.getLevel(), true);
-                    if (success) break;
-                }
-            }
-            for (ItemStack stack : player.getInventory().items) {
-                if (!stack.isEmpty() && stack.getItem() instanceof ManaItem manaItem
-                        && manaItem.canReceiveManaFromItem(stack)) {
-                    success = ManaItemHandler.instance().dispatchManaExact(stack, player, MANA * modifier.getLevel(), true);
-                    if (success) break;
-                }
-            }if (success) {
+            if (ManaItemHandler.INSTANCE.requestManaExactForTool(stack, player, MANA * modifier.getLevel(), true)) {
                 player.heal(1.0F * modifier.getLevel());
             }
         }
