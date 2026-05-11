9aae1bd3fc9795d41f0e6eacb3392e667d1b0fae
uraniniteresidue <2549847059@qq.com>
2026-05-12 00:03:33 +0800

    翻译 换了钩子，现在热解掉耐久没有那么夸张了

--- src/main/java/slimeknights/tconstruct/tools/modifiers/traits/general/ThermalDecompositeModifier.java
+++ src/main/java/slimeknights/tconstruct/tools/modifiers/traits/general/ThermalDecompositeModifier.java
@@ -9,30 +9,30 @@ import net.minecraft.world.entity.LivingEntity;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
-import slimeknights.tconstruct.library.modifiers.hook.armor.OnAttackedModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.armor.ModifyDamageModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHookMap.Builder;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
-public class ThermalDecompositeModifier extends Modifier implements OnAttackedModifierHook {
+public class ThermalDecompositeModifier extends Modifier implements ModifyDamageModifierHook {
     @Override
     protected void registerHooks(Builder hookBuilder) {
         super.registerHooks(hookBuilder);
-        hookBuilder.addHook(this, ModifierHooks.ON_ATTACKED);
+        hookBuilder.addHook(this, ModifierHooks.MODIFY_HURT);
     }
 
     @Override
-    public void onAttacked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context,
-                           EquipmentSlot slotType, DamageSource source,
-                           float amount, boolean isDirectDamage) {
+    public float modifyDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context,
+                                   EquipmentSlot slotType, DamageSource source,
+                                   float amount, boolean isDirectDamage) {
         if (source.is(DamageTypeTags.IS_FIRE)) {
             LivingEntity target = context.getEntity();
-            int damageAmount = modifier.getLevel() * 5;
+            int damageAmount = modifier.getLevel() * 20;
+            int duration = modifier.getLevel() * 100;
 
-            target.addEffect(new MobEffectInstance(MobEffects.POISON, 60, modifier.getLevel(), false, false, true));
+            target.addEffect(new MobEffectInstance(MobEffects.POISON, duration, modifier.getLevel() - 1, false, false, true));
             tool.setDamage(tool.getDamage() + damageAmount);
-
-
         }
+        return amount;
     }
 }
