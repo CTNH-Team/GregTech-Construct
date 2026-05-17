8d5e32914ba00445d113f8be6bbff705e779980d
mmyddd <mmydddd@gmail.com>
2026-05-17 14:03:49 +0800

    将manafix和terrafix改为可选

--- src/main/java/slimeknights/tconstruct/common/data/tags/ModifierTagProvider.java
+++ src/main/java/slimeknights/tconstruct/common/data/tags/ModifierTagProvider.java
@@ -73,11 +73,11 @@ public class ModifierTagProvider extends AbstractModifierTagProvider {
     this.tag(GENERAL_UPGRADES).add(
       ModifierIds.diamond, ModifierIds.emerald, ModifierIds.netherite,
       ModifierIds.reinforced, ModifierIds.overforced, ModifierIds.soulbound,
-      ModifierIds.experienced, ModifierIds.manafix, ModifierIds.terrarecover, TinkerModifiers.magnetic.getId(), ModifierIds.scope, ModifierIds.zoom,
+      ModifierIds.experienced,TinkerModifiers.magnetic.getId(), ModifierIds.scope, ModifierIds.zoom,
       ModifierIds.tank, ModifierIds.smelting, TinkerModifiers.fireprimer.getId())
         .addOptional(ModifierIds.theOneProbe);
     if (Util.isModLoaded("botania")) {
-      this.tag(GENERAL_UPGRADES).add(TinkerModifiers.manafix.getId(), TinkerModifiers.terrarecover.getId());
+      this.tag(GENERAL_UPGRADES).add(ModifierIds.manafix, ModifierIds.terrarecover);
     }
 
     this.tag(MELEE_UPGRADES).add(
