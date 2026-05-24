144a3581066feb8d5cf28417206e2e8f59c7ce0e
mmyddd <98249707+mmyddd@users.noreply.github.com>
2026-05-24 19:16:17 +0800

    Update IMaterialRecipeHelper.java

--- src/main/java/slimeknights/tconstruct/library/data/recipe/IMaterialRecipeHelper.java
+++ src/main/java/slimeknights/tconstruct/library/data/recipe/IMaterialRecipeHelper.java
@@ -4,6 +4,7 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.crafting.conditions.OrCondition;
 import net.minecraftforge.fluids.FluidStack;
 import slimeknights.mantle.recipe.data.IRecipeHelper;
@@ -80,6 +81,8 @@ public interface IMaterialRecipeHelper extends IRecipeHelper {
         wrapped = optional ? withCondition(consumer, tagCondition("storage_blocks/" + name)) : consumer;
         materialRecipe(wrapped, material, Ingredient.of(getItemTag(COMMON, "storage_blocks/" + name)), 9, 1, ItemOutput.fromTag(ingotTag), folder + matName + "/block");
     }
+
+    // ---------- 使用 FluidObject 的方法 (dev 分支) ----------
 
     /** Adds recipes to melt a material */
     default void materialMelting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, FluidObject<?> fluid, int fluidAmount, String folder) {
@@ -164,5 +167,42 @@ public interface IMaterialRecipeHelper extends IRecipeHelper {
     /** Adds recipes to melt and cast a material of ingot size */
     default void materialComposite(Consumer<FinishedRecipe> consumer, MaterialVariantId input, MaterialVariantId output, FluidObject<?> fluid, int amount, String folder) {
         materialComposite(consumer, input, output, fluid, amount, folder, output.getLocation('_').getPath());
+    }
+
+    // ---------- 直接使用 Fluid 的便利方法 (从旧分支补充) ----------
+
+    /** Adds recipes to melt a material (direct Fluid) */
+    default void materialMelting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, int fluidAmount, String folder) {
+        MaterialMeltingRecipeBuilder.material(material, fluid, fluidAmount)
+                .save(consumer, location(folder + fluid.getRegistryName().getPath() + "/melting/" + material.getLocation('_').getPath()));
+    }
+
+    /** Adds recipes to melt a material of ingot size (direct Fluid) */
+    default void materialMelting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, String folder) {
+        materialMelting(consumer, material, fluid, FluidValues.INGOT, folder);
+    }
+
+    /** Adds recipes to cast a material (direct Fluid) */
+    default void materialCasting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, int fluidAmount, String folder) {
+        MaterialFluidRecipeBuilder.material(material)
+                .setFluid(FluidIngredient.of(new FluidStack(fluid, fluidAmount)))
+                .setTemperature(getTemperature(new FluidStack(fluid, fluidAmount)))
+                .save(consumer, location(folder + fluid.getRegistryName().getPath() + "/casting/" + material.getLocation('_').getPath()));
+    }
+
+    /** Adds recipes to cast a material of ingot size (direct Fluid) */
+    default void materialCasting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, String folder) {
+        materialCasting(consumer, material, fluid, FluidValues.INGOT, folder);
+    }
+
+    /** Adds recipes to melt and cast a material (direct Fluid) */
+    default void materialMeltingCasting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, int fluidAmount, String folder) {
+        materialCasting(consumer, material, fluid, fluidAmount, folder);
+        materialMelting(consumer, material, fluid, fluidAmount, folder);
+    }
+
+    /** Adds recipes to melt and cast a material of ingot size (direct Fluid) */
+    default void materialMeltingCasting(Consumer<FinishedRecipe> consumer, MaterialVariantId material, Fluid fluid, String folder) {
+        materialMeltingCasting(consumer, material, fluid, FluidValues.INGOT, folder);
     }
 }
