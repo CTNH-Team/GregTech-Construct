7d23ad18b1c64c1fd6807ec756c5f3dd09104691
mmyddd <mmydddd@gmail.com>
2026-08-03 12:14:53 +0800

    feat(create): 为焦黑熔化炉添加烈焰人燃烧室加热提示
    - 在焦黑熔化炉物品 tooltip 中追加橙色提示，说明可由烈焰人燃烧室加热
    
    - 仅通过 Create addon 注册提示事件，未安装机械动力时无影响
    
    - 补充 tooltip.tconstruct.create.blaze_burner 中英文语言键

--- src/main/java/slimeknights/tconstruct/plugin/create/CreateTiCAddon.java
+++ src/main/java/slimeknights/tconstruct/plugin/create/CreateTiCAddon.java
@@ -3,6 +3,13 @@
  */
 package slimeknights.tconstruct.plugin.create;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 import slimeknights.tconstruct.plugin.create.modifier.CreateCrushingModifier;
 import slimeknights.tconstruct.plugin.create.modifier.CreateExtendoModifier;
 import slimeknights.tconstruct.plugin.create.modifier.CreateModifierIds;
@@ -24,6 +31,22 @@ import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 public class CreateTiCAddon implements ITiCAddon, ITiCStaticModifierAddon {
 
     public static final String MOD_ID = "create";
+
+    /** Tooltip shown on blocks that can be heated by a Blaze Burner */
+    private static final Component BLAZE_BURNER_TOOLTIP = TConstruct.makeTranslation("tooltip", "create.blaze_burner")
+            .withStyle(ChatFormatting.GOLD);
+
+    public CreateTiCAddon() {
+        MinecraftForge.EVENT_BUS.addListener(CreateTiCAddon::onTooltip);
+    }
+
+    /** Adds the Blaze Burner heating tooltip to the seared melter */
+    private static void onTooltip(ItemTooltipEvent event) {
+        if (event.getItemStack().getItem() instanceof BlockItem blockItem
+                && blockItem.getBlock() == TinkerSmeltery.searedMelter.get()) {
+            event.getToolTip().add(BLAZE_BURNER_TOOLTIP);
+        }
+    }
 
     @Override
     public String addonModId() {
