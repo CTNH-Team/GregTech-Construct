b28741a241120b2feddbe045e55b9223d7f134ba
mmyddd <mmydddd@gmail.com>
2026-08-16 14:41:33 +0800

    修正燃料速率

--- src/main/java/slimeknights/tconstruct/smeltery/data/SmelteryRecipeProvider.java
+++ src/main/java/slimeknights/tconstruct/smeltery/data/SmelteryRecipeProvider.java
@@ -1822,36 +1822,36 @@ public class SmelteryRecipeProvider extends BaseRecipeProvider implements ISmelt
         MeltingFuelBuilder.fuel(new FluidStack(TinkerFluids.blazingBlood.get(), 50), 150)
                 .save(consumer, location(folder + "fuel/blaze"));
         MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(GTMaterials.BioDiesel.getFluid(), 25)), 100, 2300)
-          .rate(25)
+          .rate(3)
           .save(consumer, location("bio_diesel"));
         // 柴油: duration=100, rate=50, temperature=3500, amount=5
         MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(GTMaterials.Diesel.getFluid(), 5)), 100, 3500)
-          .rate(50)
+          .rate(5)
           .save(consumer, location("diesel"));
         // 高十六烷值柴油: duration=200, rate=75, temperature=5000, amount=1
         MeltingFuelBuilder
           .fuel(FluidIngredient.of(new FluidStack(GTMaterials.CetaneBoostedDiesel.getFluid(), 1)), 200, 5000)
-          .rate(75)
+          .rate(7)
           .save(consumer, location("cetane_boosted_diesel"));
         // 汽油: duration=100, rate=125, temperature=3800, amount=25
         MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(GTMaterials.Gasoline.getFluid(), 25)), 100, 3800)
-          .rate(125)
+          .rate(15)
           .save(consumer, location("gasoline"));
         // 高辛烷值汽油: duration=200, rate=200, temperature=4700, amount=1
         MeltingFuelBuilder
           .fuel(FluidIngredient.of(new FluidStack(GTMaterials.HighOctaneGasoline.getFluid(), 1)), 200, 4700)
-          .rate(200)
+          .rate(2)
           .save(consumer, location("high_octane_gasoline"));
         // 烈焰流体: duration=200, rate=30, temperature=4000, amount=100
         MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(GTMaterials.Blaze.getFluid(), 100)), 200, 4000)
-          .rate(30)
+          .rate(3)
           .save(consumer, location("blaze"));
         // 烈焰之炽焱: duration=200, rate=500, temperature=5700, amount=1
         //MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(CTNHMaterials.Pyrotheum.getFluid(), 1)), 200, 5700)
         //  .rate(500)
         //  .save(consumer, location("pyrotheum"));
         MeltingFuelBuilder.fuel(FluidIngredient.of(new FluidStack(GTMaterials.Creosote.getFluid(), 50)), 200, 800)
-          .rate(500)
+          .rate(2)
           .save(consumer, location("creosote"));
         // 雪城钢: duration=114514, rate=1919, temperature=8100, amount=999
         //MeltingFuelBuilder.fuel(new FluidStack(CTNHMaterials.SNOW_STEEL.getFluid(), 999), 114514)
