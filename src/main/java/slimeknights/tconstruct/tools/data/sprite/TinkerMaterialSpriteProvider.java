e23f32c6a43e8c2d83bbc1bf0ff3d9592688bdde
mmyddd <mmydddd@gmail.com>
2026-09-07 01:15:58 +0800

    fix(materials): 补齐史莱姆护甲覆盖贴图与珠宝皮革配色
    - 补齐 overlay_armor/overlay_armor_bone/overlay_leggings 基础贴图
    - 修正 jeweledHide 精灵调色板为上游青色系

--- src/main/java/slimeknights/tconstruct/tools/data/sprite/TinkerMaterialSpriteProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/sprite/TinkerMaterialSpriteProvider.java
@@ -546,7 +546,7 @@ public class TinkerMaterialSpriteProvider extends AbstractMaterialSpriteProvider
         buildMaterial(MaterialIds.jeweledHide)
                 .fallbacks("cloth")
                 .statType(StatlessMaterialStats.BINDING, StatlessMaterialStats.BOWSTRING).cuirass().maille().laces()
-                .colorMapper(GreyToColorMapping.builderFromBlack().addARGB(63, 0xFF433E3D).addARGB(102, 0xFF494845).addARGB(140, 0xFF595959).addARGB(178, 0xFF666460).addARGB(216, 0xFF817E76).addARGB(255, 0xFFA3A393).build());
+                .colorMapper(GreyToColorMapping.builderFromBlack().addARGB(63, 0xFF145E53).addARGB(102, 0xFF11727A).addARGB(140, 0xFF20C5B5).addARGB(178, 0xFF4AEDD9).addARGB(216, 0xFFA1FBE8).addARGB(255, 0xFFD5FFF6).build());
         buildMaterial(MaterialIds.enderslimeVine)
                 .statType(StatlessMaterialStats.BINDING, StatlessMaterialStats.BOWSTRING).cuirass().maille()
                 .armorLinear()
