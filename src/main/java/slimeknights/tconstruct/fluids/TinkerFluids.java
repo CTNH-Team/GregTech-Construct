ebbaa83e39a59e2c11390412ac1f0a8dc0fd27db
mmyddd <mmydddd@gmail.com>
2026-09-07 01:36:37 +0800

    refactor(mantle): 迁移灵魂绑定与战斗辅助至 Mantle 基座
    - soulbound 槽位标记改用 MantleEvents.SOULBOUND_SLOT 并删除本地恢复逻辑
    - ToolAttackUtil 改用 CombatHelper 属性合并与主手堆叠
    - 瓶子标签改用 MantleTags 并保留 TiC 旧键 deprecated 别名

--- src/main/java/slimeknights/tconstruct/fluids/TinkerFluids.java
+++ src/main/java/slimeknights/tconstruct/fluids/TinkerFluids.java
@@ -35,6 +35,7 @@ import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.RegistryObject;
+import slimeknights.mantle.datagen.MantleTags;
 import slimeknights.mantle.fluid.InvertedFluid;
 import slimeknights.mantle.fluid.UnplaceableFluid;
 import slimeknights.mantle.registration.RegistrationHelper;
@@ -276,7 +277,7 @@ public final class TinkerFluids extends TinkerModule {
             CauldronInteraction.EMPTY.put(Items.LINGERING_POTION, new EmptyBottleIntoEmpty(lingeringBottle, CauldronInteraction.EMPTY.get(Items.LINGERING_POTION)));
             // brew bottles into each other, bit weird but feels better than shapeless
             BrewingRecipeRegistry.addRecipe(new BottleBrewingRecipe(Ingredient.of(Items.GLASS_BOTTLE), Items.POTION, Items.SPLASH_POTION, new ItemStack(splashBottle)));
-            BrewingRecipeRegistry.addRecipe(new BottleBrewingRecipe(Ingredient.of(TinkerTags.Items.SPLASH_BOTTLE), Items.SPLASH_POTION, Items.LINGERING_POTION, new ItemStack(lingeringBottle)));
+            BrewingRecipeRegistry.addRecipe(new BottleBrewingRecipe(Ingredient.of(MantleTags.Items.SPLASH_BOTTLE), Items.SPLASH_POTION, Items.LINGERING_POTION, new ItemStack(lingeringBottle)));
         });
 
         // dispense buckets
